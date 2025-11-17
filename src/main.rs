49c28726ae9e08c78752cb482a2e2351083aae52
jan-kozlowski <jankoozlowskiski@gmail.com>
2025-11-17 13:28:07 +0100

    zadanie 4 start

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-use std::{collections::{HashMap, HashSet}, io::{Read, Write}};
+use std::{
+    collections::{HashMap, HashSet},
+    io::{Read, Write},
+};
 
 #[derive(PartialEq)]
 struct Autorstwo {
-
     praca: u16,
-    autor: String
+    autor: String,
 }
 
 #[derive(Debug)]
 struct Autor {
-
     id: String,
     ryzyko: u8,
-    sloty: String
+    sloty: String,
 }
 
 #[derive(PartialEq)]
 struct Praca {
-
     id: u16,
     tytul: String,
     rok: u16,
     autorzy: u8,
-    punkty: u8
+    punkty: u8,
 }
 
 const AUTORSTWA_ROZMIAR: usize = 630;
@@ -30,20 +30,20 @@ const AUTORZY_ROZMIAR: usize = 125;
 const PRACE_ROZMIAR: usize = 389;
 
 fn wczytaj_autorstwo() -> Result<Vec<Autorstwo>, String> {
-
     let mut autorstwa = Vec::new();
-    let mut input_file = std::fs::File::open("./assets/Autorstwo.csv")
-        .map_err(|e| e.to_string())?;
+    let mut input_file =
+        std::fs::File::open("./assets/Autorstwo.csv").map_err(|e| e.to_string())?;
 
     let mut rows_parsed = 0;
     let mut lines = String::new();
-    input_file.read_to_string(&mut lines)
+    input_file
+        .read_to_string(&mut lines)
         .map_err(|e| e.to_string())?;
 
     for line in lines.lines() {
-
         // let (id, tytul, rok, autorzy, punkty ) = get_line_data(line).ok_or(format!("Błąd konwertowania wiersza {}", rows_selected + 1))?;
-        let autorstwo = get_autorstwo(line).ok_or(format!("Błąd konwertowania wiersza {}", rows_parsed + 1))?;
+        let autorstwo =
+            get_autorstwo(line).ok_or(format!("Błąd konwertowania wiersza {}", rows_parsed + 1))?;
         autorstwa.push(autorstwo);
         rows_parsed += 1;
     }
@@ -54,20 +54,19 @@ fn wczytaj_autorstwo() -> Result<Vec<Autorstwo>, String> {
 }
 
 fn wczytaj_autorow() -> Result<Vec<Autor>, String> {
-
     let mut autorzy = Vec::new();
-    let mut input_file = std::fs::File::open("./assets/Autorzy.csv")
-        .map_err(|e| e.to_string())?;
+    let mut input_file = std::fs::File::open("./assets/Autorzy.csv").map_err(|e| e.to_string())?;
 
-    let mut rows_parsed= 0;
+    let mut rows_parsed = 0;
     let mut lines = String::new();
-    input_file.read_to_string(&mut lines)
+    input_file
+        .read_to_string(&mut lines)
         .map_err(|e| e.to_string())?;
 
     for line in lines.lines() {
-
         // let (id, tytul, rok, autorzy, punkty ) = get_line_data(line).ok_or(format!("Błąd konwertowania wiersza {}", rows_selected + 1))?;
-        let autor = get_autor(line).ok_or(format!("Błąd konwertowania wiersza {}", rows_parsed + 1))?;
+        let autor =
+            get_autor(line).ok_or(format!("Błąd konwertowania wiersza {}", rows_parsed + 1))?;
         autorzy.push(autor);
         rows_parsed += 1;
     }
@@ -78,19 +77,18 @@ fn wczytaj_autorow() -> Result<Vec<Autor>, String> {
 }
 
 fn wczytaj_prace() -> Result<HashMap<u16, Praca>, String> {
-
     let mut prace = HashMap::new();
-    let mut input_file = std::fs::File::open("./assets/Prace.csv")
-        .map_err(|e| e.to_string())?;
+    let mut input_file = std::fs::File::open("./assets/Prace.csv").map_err(|e| e.to_string())?;
 
-    let mut rows_parsed= 0;
+    let mut rows_parsed = 0;
     let mut lines = String::new();
-    input_file.read_to_string(&mut lines)
+    input_file
+        .read_to_string(&mut lines)
         .map_err(|e| e.to_string())?;
 
     for line in lines.lines() {
-
-        let praca = get_praca(line).ok_or(format!("Błąd konwertowania wiersza {}", rows_parsed + 1))?;
+        let praca =
+            get_praca(line).ok_or(format!("Błąd konwertowania wiersza {}", rows_parsed + 1))?;
         prace.insert(praca.id, praca);
         rows_parsed += 1;
     }
@@ -101,12 +99,16 @@ fn wczytaj_prace() -> Result<HashMap<u16, Praca>, String> {
     Ok(prace)
 }
 
-fn make_query_file_2(autorstwa: &Vec<Autorstwo>, autorzy: &Vec<Autor>, prace: &HashMap<u16, Praca>) -> Result<(), std::io::Error> {
-
+fn make_query_file_2(
+    autorstwa: &Vec<Autorstwo>,
+    autorzy: &Vec<Autor>,
+    prace: &HashMap<u16, Praca>,
+) -> Result<(), std::io::Error> {
     let mut output_file = std::fs::File::create("./assets/Publikacje_2.sql")?;
 
-    writeln!(&mut output_file,
-    "CREATE TABLE autorstwa (
+    writeln!(
+        &mut output_file,
+        "CREATE TABLE autorstwa (
     praca NUMBER(3, 0) NOT NULL,
     autor VARCHAR2(29) NOT NULL,
     PRIMARY KEY (praca, autor)
@@ -124,7 +126,8 @@ CREATE TABLE prace (
     rok NUMBER(4, 0) NOT NULL,
     autorzy NUMBER(2, 0) NOT NULL,
     punkty NUMBER(3, 0) NOT NULL
-);")?;
+);"
+    )?;
 
     writeln!(&mut output_file)?;
 
@@ -133,18 +136,22 @@ CREATE TABLE prace (
     let mut prace_selected = 0;
 
     for autorstwo in autorstwa {
-
-        writeln!(&mut output_file, "INSERT INTO autorstwa (praca, autor) VALUES ({}, '{}');",
-            autorstwo.praca, autorstwo.autor)?;
+        writeln!(
+            &mut output_file,
+            "INSERT INTO autorstwa (praca, autor) VALUES ({}, '{}');",
+            autorstwo.praca, autorstwo.autor
+        )?;
         autorstwa_selected += 1;
     }
 
     writeln!(&mut output_file)?;
 
     for autor in autorzy {
-
-        writeln!(&mut output_file, "INSERT INTO autorzy (autor, ryzyko, sloty) VALUES ('{}', {}, '{}');",
-            autor.id, autor.ryzyko, autor.sloty)?;
+        writeln!(
+            &mut output_file,
+            "INSERT INTO autorzy (autor, ryzyko, sloty) VALUES ('{}', {}, '{}');",
+            autor.id, autor.ryzyko, autor.sloty
+        )?;
 
         autorzy_selected += 1;
     }
@@ -152,9 +159,11 @@ CREATE TABLE prace (
     writeln!(&mut output_file)?;
 
     for praca in prace.values() {
-
-        writeln!(&mut output_file, "INSERT INTO prace (id, tytul, rok, autorzy, punkty) VALUES ({}, '{}', {}, {}, {});",
-            praca.id, praca.tytul, praca.rok, praca.autorzy, praca.punkty)?;
+        writeln!(
+            &mut output_file,
+            "INSERT INTO prace (id, tytul, rok, autorzy, punkty) VALUES ({}, '{}', {}, {}, {});",
+            praca.id, praca.tytul, praca.rok, praca.autorzy, praca.punkty
+        )?;
 
         prace_selected += 1;
     }
@@ -179,70 +188,67 @@ fn publikacje_2() -> Result<(), String> {
     let mut publikacje = HashMap::new();
 
     for autor in &autorzy {
-
-        if autor.ryzyko != 1 { continue }
+        if autor.ryzyko != 1 {
+            continue;
+        }
 
         let mut jest_wybrany = true;
         let mut publikacje_autora = Vec::new();
 
         for autorstwo in &autorstwa {
-
-            if autorstwo.autor != autor.id { continue; }
+            if autorstwo.autor != autor.id {
+                continue;
+            }
 
             let praca = prace.get(&autorstwo.praca).ok_or("Brak pracy")?;
             publikacje_autora.push(praca);
 
             if praca.rok == 2020 {
-
                 jest_wybrany = false;
             }
         }
 
         if jest_wybrany && publikacje_autora.len() > 0 {
-
             publikacje.insert(autor.id.clone(), publikacje_autora);
         }
     }
 
     let mut lines = 0;
     for autor in publikacje.keys() {
-
         println!("wybrany: {}", autor);
     }
     println!("liczba wybranych: {}", publikacje.len());
 
     for (autor, publikacje_autora) in publikacje {
-
         let mut wspolpracownicy = HashSet::new();
 
         for autorstwo in &autorstwa {
-
             for publikacja in &publikacje_autora {
-
                 if autorstwo.praca == publikacja.id && autorstwo.autor != autor {
-
                     wspolpracownicy.insert(&autorstwo.autor);
                 }
             }
         }
 
         for wspolpracownik in wspolpracownicy {
-
             lines += 1;
             println!("autor: {}, współpracownik: {}", autor, wspolpracownik);
         }
     }
     println!("wypisano: {lines}");
 
-    make_query_file_2(&autorstwa, &autorzy, &prace)
-        .map_err(|e| e.to_string())
+    make_query_file_2(&autorstwa, &autorzy, &prace).map_err(|e| e.to_string())
 }
 
-fn make_query_file_3(autorstwa: &Vec<Autorstwo>, autorzy: &Vec<Autor>, prace: &HashMap<u16, Praca>) -> Result<(), std::io::Error> {
-
+fn make_query_file_3(
+    autorstwa: &Vec<Autorstwo>,
+    autorzy: &Vec<Autor>,
+    prace: &HashMap<u16, Praca>,
+) -> Result<(), std::io::Error> {
     let mut output_file = std::fs::File::create("./assets/Publikacje_3.sql")?;
 
-    writeln!(&mut output_file,
+    writeln!(
+        &mut output_file,
         "CREATE TABLE autorstwa (
     praca NUMBER(3, 0) NOT NULL,
     autor VARCHAR2(29) NOT NULL,
@@ -261,7 +267,8 @@ CREATE TABLE prace (
     rok NUMBER(4, 0) NOT NULL,
     autorzy NUMBER(2, 0) NOT NULL,
     punkty NUMBER(3, 0) NOT NULL
-);")?;
+);"
+    )?;
 
     writeln!(&mut output_file)?;
 
@@ -270,18 +277,22 @@ CREATE TABLE prace (
     let mut prace_selected = 0;
 
     for autorstwo in autorstwa {
-
-        writeln!(&mut output_file, "INSERT INTO autorstwa (praca, autor) VALUES ({}, '{}');",
-            autorstwo.praca, autorstwo.autor)?;
+        writeln!(
+            &mut output_file,
+            "INSERT INTO autorstwa (praca, autor) VALUES ({}, '{}');",
+            autorstwo.praca, autorstwo.autor
+        )?;
         autorstwa_selected += 1;
     }
 
     writeln!(&mut output_file)?;
 
     for autor in autorzy {
-
-        writeln!(&mut output_file, "INSERT INTO autorzy (autor, ryzyko, sloty) VALUES ('{}', {}, '{}');",
-            autor.id, autor.ryzyko, autor.sloty)?;
+        writeln!(
+            &mut output_file,
+            "INSERT INTO autorzy (autor, ryzyko, sloty) VALUES ('{}', {}, '{}');",
+            autor.id, autor.ryzyko, autor.sloty
+        )?;
 
         autorzy_selected += 1;
     }
@@ -289,9 +300,11 @@ CREATE TABLE prace (
     writeln!(&mut output_file)?;
 
     for praca in prace.values() {
-
-        writeln!(&mut output_file, "INSERT INTO prace (id, tytul, rok, autorzy, punkty) VALUES ({}, '{}', {}, {}, {});",
-            praca.id, praca.tytul, praca.rok, praca.autorzy, praca.punkty)?;
+        writeln!(
+            &mut output_file,
+            "INSERT INTO prace (id, tytul, rok, autorzy, punkty) VALUES ({}, '{}', {}, {}, {});",
+            praca.id, praca.tytul, praca.rok, praca.autorzy, praca.punkty
+        )?;
 
         prace_selected += 1;
     }
@@ -313,7 +326,6 @@ DROP TABLE prace;"
 }
 
 fn publikacje_3() -> Result<(), String> {
-
     let autorstwa = wczytaj_autorstwo()?;
     let autorzy = wczytaj_autorow()?;
     let prace = wczytaj_prace()?;
@@ -321,18 +333,14 @@ fn publikacje_3() -> Result<(), String> {
 
     let mut wartosci = HashMap::new();
     for (id, praca) in &prace {
-
         wartosci.insert(id, praca.punkty as f32 / praca.autorzy as f32);
     }
 
     let mut prace_autora = HashMap::new();
     for autor in &autorzy {
-
         let mut publikacje = Vec::new();
         for autorstwo in &autorstwa {
-
             if autorstwo.autor == autor.id {
-
                 let wartosc = wartosci.get(&autorstwo.praca).ok_or("Błąd")?.clone();
                 publikacje.push((autorstwo.praca, wartosc));
                 inserted += 1;
@@ -345,13 +353,9 @@ fn publikacje_3() -> Result<(), String> {
         let mut odrzucone = Vec::new();
 
         for publikacja in publikacje {
-
             if wybrane.len() < 4 {
-
                 wybrane.push(publikacja);
-
             } else {
-
                 odrzucone.push(publikacja);
             }
         }
@@ -363,20 +367,17 @@ fn publikacje_3() -> Result<(), String> {
     let mut discarded = 0;
     let mut printed = 0;
     for autor in &autorzy {
-
         let (wybrane, odrzucone) = prace_autora.get(&autor.id).ok_or("Błąd")?;
         let mut wynik_autora = 0.;
         assert!(wybrane.len() <= 4);
         selected += wybrane.len();
         discarded += odrzucone.len();
         for (id, wartosc) in wybrane {
-
             wynik_autora += wartosc;
             // println!("autor: {}, praca: {}, wartosc: {}",
             //     autor.id, id, wartosc);
 
             for odrzucona in odrzucone {
-
                 assert!(wartosc.clone() >= odrzucona.1);
             }
         }
@@ -388,7 +389,6 @@ fn publikacje_3() -> Result<(), String> {
         // }
 
         if wybrane.len() > 0 {
-
             printed += 1;
             println!("AUTOR: {}, WYNIK: {}", autor.id, wynik_autora);
         }
@@ -402,17 +402,185 @@ fn publikacje_3() -> Result<(), String> {
     println!("total: {}", selected + discarded);
     println!("wypisano: {printed}");
 
-    make_query_file_3(&autorstwa, &autorzy, &prace)
-        .map_err(|e| e.to_string())
+    make_query_file_3(&autorstwa, &autorzy, &prace).map_err(|e| e.to_string())
+}
+
+fn make_query_file_4(
+    autorstwa: &Vec<Autorstwo>,
+    autorzy: &Vec<Autor>,
+    prace: &HashMap<u16, Praca>,
+) -> Result<(), std::io::Error> {
+    let mut output_file = std::fs::File::create("./assets/Publikacje_4.sql")?;
+
+    writeln!(
+        &mut output_file,
+        "CREATE TABLE autorstwa (
+    praca NUMBER(3, 0) NOT NULL,
+    autor VARCHAR2(29) NOT NULL,
+    PRIMARY KEY (praca, autor)
+);
+
+CREATE TABLE autorzy (
+    autor VARCHAR2(29) PRIMARY KEY,
+    ryzyko NUMBER(1, 0) NOT NULL,
+    sloty VARCHAR2(4) NOT NULL
+);
+
+CREATE TABLE prace (
+    id NUMBER(3, 0) PRIMARY KEY,
+    tytul VARCHAR2(197) NOT NULL,
+    rok NUMBER(4, 0) NOT NULL,
+    autorzy NUMBER(2, 0) NOT NULL,
+    punkty NUMBER(3, 0) NOT NULL
+);"
+    )?;
+
+    writeln!(&mut output_file)?;
+
+    let mut autorstwa_selected = 0;
+    let mut autorzy_selected = 0;
+    let mut prace_selected = 0;
+
+    for autorstwo in autorstwa {
+        writeln!(
+            &mut output_file,
+            "INSERT INTO autorstwa (praca, autor) VALUES ({}, '{}');",
+            autorstwo.praca, autorstwo.autor
+        )?;
+        autorstwa_selected += 1;
+    }
+
+    writeln!(&mut output_file)?;
+
+    for autor in autorzy {
+        writeln!(
+            &mut output_file,
+            "INSERT INTO autorzy (autor, ryzyko, sloty) VALUES ('{}', {}, '{}');",
+            autor.id, autor.ryzyko, autor.sloty
+        )?;
+
+        autorzy_selected += 1;
+    }
+
+    writeln!(&mut output_file)?;
+
+    for praca in prace.values() {
+        writeln!(
+            &mut output_file,
+            "INSERT INTO prace (id, tytul, rok, autorzy, punkty) VALUES ({}, '{}', {}, {}, {});",
+            praca.id, praca.tytul, praca.rok, praca.autorzy, praca.punkty
+        )?;
+
+        prace_selected += 1;
+    }
+
+    writeln!(&mut output_file,
+    "
+SELECT autor, SUM(wartosc) as wynik FROM (SELECT A.autor, praca, W.id, W.wartosc, ROW_NUMBER() OVER (PARTITION BY A.autor ORDER BY W.wartosc desc) as rn FROM autorzy A JOIN autorstwa ON A.autor = autorstwa.autor JOIN (SELECT id, (punkty / autorzy) AS wartosc from prace) W ON W.id = autorstwa.praca) WHERE rn <= 4 GROUP BY autor;
+
+DROP TABLE autorzy;
+DROP TABLE autorstwa;
+DROP TABLE prace;"
+    )?;
+
+    assert_eq!(autorstwa_selected, AUTORSTWA_ROZMIAR);
+    assert_eq!(autorzy_selected, AUTORZY_ROZMIAR);
+    assert_eq!(prace_selected, PRACE_ROZMIAR);
+
+    Ok(())
+}
+
+fn policz(
+    liczby: &mut HashMap<String, i32>,
+    imie_autora: String,
+    obliczeni: &mut HashSet<String>,
+    autorstwa: &Vec<Autorstwo>,
+    wartosc: i32,
+) {
+    assert!(obliczeni.contains(&imie_autora));
+
+    let mut prace_autora = Vec::new();
+    let mut wspolautorzy = Vec::new();
+
+    for autorstwo in autorstwa {
+        if autorstwo.autor == imie_autora.clone() {
+            prace_autora.push(autorstwo.praca);
+        }
+    }
+
+    for praca_autora in prace_autora {
+        for autorstwo in autorstwa {
+            if autorstwo.praca != praca_autora || obliczeni.contains(&autorstwo.autor) {
+                continue;
+            }
+            assert_ne!(autorstwo.autor, imie_autora);
+            wspolautorzy.push(autorstwo.autor.clone());
+            liczby.insert(autorstwo.autor.clone(), wartosc + 1);
+            obliczeni.insert(autorstwo.autor.clone());
+        }
+    }
+
+    for wspolautor in wspolautorzy {
+        let val = liczby.get(&wspolautor).unwrap();
+        // println!("autor: {}, wartosc: {val}", imie_autora);
+        assert_eq!(val.clone(), wartosc + 1);
+        policz(liczby, wspolautor, obliczeni, autorstwa, wartosc + 1);
+    }
+}
+
+fn publikacje_4() -> Result<(), String> {
+    let autorstwa = wczytaj_autorstwo()?;
+    let autorzy = wczytaj_autorow()?;
+    let prace = wczytaj_prace()?;
+
+    let mut liczby = HashMap::new();
+    liczby.insert(String::from("Pilipczuk Mi"), 0);
+
+    policz(
+        &mut liczby,
+        String::from("Pilipczuk Mi"),
+        &mut HashSet::from([String::from("Pilipczuk Mi")]),
+        &autorstwa,
+        0,
+    );
+
+    assert_eq!(liczby.get("Pilipczuk Mi").unwrap().clone(), 0);
+    let mut prace_autora = Vec::new();
+    for autorstwo in &autorstwa {
+        if autorstwo.autor == "Pilipczuk Mi" {
+            prace_autora.push(autorstwo.praca);
+        }
+    }
+    assert_eq!(prace_autora.len(), 48);
+
+    for praca_autora in &prace_autora {
+        for autorstwo in &autorstwa {
+            if autorstwo.praca != *praca_autora || autorstwo.autor == "Pilipczuk Mi" {
+                continue;
+            }
+            assert_eq!(liczby.get(&autorstwo.autor).unwrap().clone(), 1);
+        }
+    }
+
+    for autor in &autorzy {
+        println!(
+            "{}: {}",
+            autor.id,
+            match liczby.get(&autor.id) {
+                None => "NULL",
+                Some(liczba) => &liczba.to_string(),
+            }
+        );
+    }
+
+    make_query_file_4(&autorstwa, &autorzy, &prace).map_err(|e| e.to_string())
 }
 
 fn main() -> Result<(), String> {
-
-    publikacje_3()
+    publikacje_4()
 }
 
 fn get_autorstwo(line: &str) -> Option<Autorstwo> {
-
     let mut split = line.split(',');
     let praca: u16 = split.next()?.parse().ok()?;
     let autor: String = split.next()?.parse().ok()?;
@@ -420,12 +588,14 @@ fn get_autorstwo(line: &str) -> Option<Autorstwo> {
 
     match koniec {
         Some(_v) => None,
-        None => Some(Autorstwo { praca: praca, autor: autor })
+        None => Some(Autorstwo {
+            praca: praca,
+            autor: autor,
+        }),
     }
 }
 
 fn get_autor(line: &str) -> Option<Autor> {
-
     let mut in_quotes = false;
     let mut fields = Vec::new();
     let mut current = String::new();
@@ -442,7 +612,9 @@ fn get_autor(line: &str) -> Option<Autor> {
     }
     fields.push(current.trim_matches('"').to_string());
 
-    if fields.len() != 3 { return None; }
+    if fields.len() != 3 {
+        return None;
+    }
 
     Some(Autor {
         id: fields[0].clone(),
@@ -452,7 +624,6 @@ fn get_autor(line: &str) -> Option<Autor> {
 }
 
 fn get_praca(line: &str) -> Option<Praca> {
-
     let mut split = line.split(';');
     let id: u16 = split.next()?.parse().ok()?;
     let tytul: String = split.next()?.parse().ok()?;
@@ -463,6 +634,12 @@ fn get_praca(line: &str) -> Option<Praca> {
 
     match koniec {
         Some(_v) => None,
-        None => Some(Praca { id: id, tytul: tytul, rok: rok, autorzy: autorzy, punkty: punkty })
+        None => Some(Praca {
+            id: id,
+            tytul: tytul,
+            rok: rok,
+            autorzy: autorzy,
+            punkty: punkty,
+        }),
     }
 }
